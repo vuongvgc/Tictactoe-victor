8ce847697da73f0d6a0a17a1e99f0e6bbb36b0b8
vuongvgc <vuongvgc@gmail.com>
2020-08-09 19:05:10 +0700

    test commit

--- src/index.js
+++ src/index.js
@@ -53,9 +53,7 @@ class Board extends React.Component {
           {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          
         </div>
       </div>
     );
